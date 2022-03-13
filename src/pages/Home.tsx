e7c890a6e3c02561082670b26d2a9769740408e6
Juann Pablo Brandão Santos <62782269+juannBrandao@users.noreply.github.com>
2022-03-13 19:03:32 -0300

    desafio1

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,15 +9,26 @@ export function Home() {
   const [tasks, setTasks] = useState<Task[]>([]);
 
   function handleAddTask(newTaskTitle: string) {
-    //TODO - add new task
+    const data = {
+      id: new Date().getTime(),
+      title: newTaskTitle,
+      done: false,
+    }
+    setTasks([...tasks, data])
   }
 
   function handleToggleTaskDone(id: number) {
-    //TODO - toggle task done if exists
+    const updatedTasks = [...tasks]
+    const findItem = updatedTasks.find(item=> item.id === id)
+
+    if (!findItem) return
+
+    findItem.done = !findItem.done
+    setTasks(updatedTasks)
   }
 
   function handleRemoveTask(id: number) {
-    //TODO - remove task from state
+    setTasks(onldState => onldState.filter(tasks=> tasks.id !== id))
   }
 
   return (
